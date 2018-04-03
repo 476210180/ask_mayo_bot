63b82f86ff484391732dc331bd338d488e44ad08
Mayo <mayocream39@yahoo.co.jp>
2018-04-03 18:37:08 +0800

    1818

--- settings.php
+++ settings.php
@@ -1,5 +1,10 @@
 <?php 
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+
 date_default_timezone_set('Asia/Shanghai');
 
 define('BOT_API_TOKEN', getenv('BOT_API_TOKEN'));
