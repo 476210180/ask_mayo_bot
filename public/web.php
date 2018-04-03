83d3485ed016f77b730250ddeca548091269cfe6
Mayo <mayocream39@yahoo.co.jp>
2018-04-03 18:24:58 +0800

    1818

--- public/web.php
+++ public/web.php
@@ -5,8 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 // Settings
 require __DIR__ . '/../settings.php';
 // App
-//$app = new \Bot\Core;
+$app = new \Bot\Core;
 
-//$app->run();
+$app->run();
 
 echo "Ask Mayo Anthing v0.1";
