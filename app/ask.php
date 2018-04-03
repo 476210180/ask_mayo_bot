d2a45d5325c78368aa79ad3f171aa7c9a705dd0f
Mayo <mayocream39@yahoo.co.jp>
2018-04-03 19:15:40 +0800

    1818

--- app/ask.php
+++ app/ask.php
@@ -14,7 +14,7 @@ class Ask {
 		$message = trim(Di::get('message_text'));
 		$chat_id = base64_encode(Di::get('update')['message']['from']['id']));
 		$data = [
-			'chat_id' => (int)MASTER_CHAT_ID,
+			'chat_id' => '546902537',
 			'text' => "{$chat_id}  " . date('H:i') . "\n" . $message
 		];
 		return Di::get('telegram')->sendMessage($data);
